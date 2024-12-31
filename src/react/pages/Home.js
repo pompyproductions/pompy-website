931cd892ea1e3ae3d180d5b9a5903e75ea54599b
Eren Gazioglu <pompyproduction@gmail.com>
2024-12-31 18:29:35 +0100

    Basic WIP page structure

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
+import RoundLogo from "../../assets/round-logo.svg"
 
 const Home = () => {
   return (
     <main>
-      <section className="card">
-        <h2>Styling</h2>
-        <p>Text explaining styling.</p>
+      <section>
+        <RoundLogo id="round-logo"/>
+        <h1 className="single-margin">
+          This website is under construction.
+        </h1>
+        <div className="vsep double-margin"></div>
+        <p className="single-margin">
+          For business enquiries & information, please get in touch by sending a mail to info@pompyproductions.com.
+        </p>
+        <p className="single-margin">
+          If you’d like to be informed once the website is up and running, enter your address below, and we’ll get in touch with you!
+        </p>
       </section>
       <div className="logo">
         <span className="outline-text">POMPY</span>
